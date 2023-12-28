d696d1e420578438234fdb6cdd79157afb9558a5
Alejandro <ian.alejandro.volume2@gmail.com>
2023-12-27 23:58:26 -0300

    Mensagem de teste

---
+++ aaa.js
@@ -0,0 +1,4 @@
+var valores = [2,3,5,3,1232,34]
+for (pos in valores){
+    console.log(valores[pos])
+}
--- javascript.js
+++
@@ -1,4 +0,0 @@
-let valores = [3, 4, 5, 1]
-for(pos in valores){
-    console.log("Valor: " + valores[pos])
-}
